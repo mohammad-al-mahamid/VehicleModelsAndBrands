e70b58366557214ca2f36159bc650784628346f3
Mohammad AL Mahamid <almahamid98@gmail.com>
2023-04-25 13:16:35 +0200

    define BaseEntity

---
+++ src/main/java/io/kprosoftware/codechallenge/common/BaseEntity.java
@@ -0,0 +1,56 @@
+package io.kprosoftware.codechallenge.common;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity<PK> {
+
+  @Id
+  @GeneratedValue
+  private PK id;
+
+  public BaseEntity() {
+
+  }
+
+  public PK getId() {
+    return id;
+  }
+
+  public void setId(PK id) {
+    this.id = id;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    BaseEntity other = (BaseEntity) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "BaseEntity [id=" + id + "]";
+  }
+
+}
