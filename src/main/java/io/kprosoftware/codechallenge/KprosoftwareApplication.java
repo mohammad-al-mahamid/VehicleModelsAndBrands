b49dd8bc4252ba88524335ba01f437317e8e6c68
Mohammad AL Mahamid <almahamid98@gmail.com>
2023-04-25 13:16:57 +0200

    fix structur

--- src/main/java/io/kprosoftware/codechallenge/KprosoftwareApplication.java
+++ src/main/java/io/kprosoftware/codechallenge/KprosoftwareApplication.java
@@ -1,4 +1,4 @@
-package io.kprosoftware.codechallenge.kprosoftware;
+package io.kprosoftware.codechallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
