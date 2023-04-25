a98140391db26dd7d97ada2dc1dd59445f3c09ff
Mohammad AL Mahamid <almahamid98@gmail.com>
2023-04-25 12:51:37 +0200

    init commit

---
+++ src/main/java/io/kprosoftware/codechallenge/kprosoftware/KprosoftwareApplication.java
@@ -0,0 +1,13 @@
+package io.kprosoftware.codechallenge.kprosoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KprosoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KprosoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kprosoftware/codechallenge/kprosoftware/KprosoftwareApplicationTests.java
@@ -0,0 +1,12 @@
+package io.kprosoftware.codechallenge.kprosoftware;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KprosoftwareApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
