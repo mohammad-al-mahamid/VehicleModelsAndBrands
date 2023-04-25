d9e8d1426b46227c63c3c1d2e939adafcc86c95f
Mohammad AL Mahamid <almahamid98@gmail.com>
2023-04-25 15:04:36 +0200

    Service for VehicleBrands

--- src/main/java/io/kprosoftware/codechallenge/service/VehicleBrandsService.java
+++ src/main/java/io/kprosoftware/codechallenge/service/VehicleBrandsService.java
@@ -1,12 +1,14 @@
 package io.kprosoftware.codechallenge.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.kprosoftware.codechallenge.entity.VehicleBrands;
+import io.kprosoftware.codechallenge.exception.VehicleBrandNotFoundExceptionException;
 import io.kprosoftware.codechallenge.repository.VehicleBrandsRepository;
 
 @Service
@@ -27,5 +29,59 @@ public class VehicleBrandsService {
       logger.info("List is Empty");
     }
     return vehicleBrands;
+  }
+
+  public VehicleBrands getVehicleBrandsById(Long id) {
+    Optional<VehicleBrands> result = vehicleBrandsRepository.findById(id);
+
+    if (!result.isPresent()) {
+      logger.info("VehicleBrands Not Found");
+
+    } else {
+      logger.info("VehicleBrands at " + id + "is" + result.get());
+      return result.get();
+    }
+    return null;
+  }
+
+  public VehicleBrands addVehicleBrands(VehicleBrands VehicleBrands) {
+    VehicleBrands newVehicleBrands = vehicleBrandsRepository.saveAndFlush(VehicleBrands);
+    logger.info("Saved VehicleBrands with id" + newVehicleBrands.getId());
+    logger.info("VehicleBrands saved" + newVehicleBrands.toString());
+    vehicleBrandsRepository.saveAndFlush(newVehicleBrands);
+    return newVehicleBrands;
+  }
+
+  public VehicleBrands UpdateVehicleBrands(VehicleBrands newVehicleBrands, Long id) {
+    return vehicleBrandsRepository.findById(id)
+        .map(VehicleBrands -> {
+          logger.info("old VehicleBrands: " + vehicleBrandsRepository.findById(id).get());
+          VehicleBrands.setName(newVehicleBrands.getName());
+          VehicleBrands.setPriceSegment(newVehicleBrands.getPriceSegment());
+
+          logger.info("replace was successful: " + VehicleBrands.toString());
+          return vehicleBrandsRepository.save(VehicleBrands);
+        })
+        .orElseThrow(() -> new VehicleBrandNotFoundExceptionException(id));
+  }
+
+  public void deleteVehicleBrandsById(Long id) {
+    Optional<VehicleBrands> result = vehicleBrandsRepository.findById(id);
+
+    if (result.isPresent()) {
+      logger.warn("vehicleBrands with id" + id + " will be removed from the Database ");
+      vehicleBrandsRepository.deleteById(id);
+    } else {
+      throw new VehicleBrandNotFoundExceptionException(id);
+    }
+  }
+
+  public void deleteALLVehicleBrands() {
+    List<VehicleBrands> result = vehicleBrandsRepository.findAll();
+
+    if (result.isEmpty())
+      throw new VehicleBrandNotFoundExceptionException();
+    logger.warn("Delete ALL VehicleBrands!!! ");
+    vehicleBrandsRepository.deleteAll();
   }
 }
