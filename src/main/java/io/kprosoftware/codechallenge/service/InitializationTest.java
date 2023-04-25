3fa71defb8f8ece43010a2f4b912f32ed78bf4bf
Mohammad AL Mahamid <almahamid98@gmail.com>
2023-04-26 01:03:44 +0200

    initDB

--- src/main/java/io/kprosoftware/codechallenge/service/InitializationTest.java
+++ src/main/java/io/kprosoftware/codechallenge/service/InitializationTest.java
@@ -2,7 +2,9 @@ package io.kprosoftware.codechallenge.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
 
+import io.kprosoftware.codechallenge.entity.Model;
 import io.kprosoftware.codechallenge.entity.VehicleBrand;
 import io.kprosoftware.codechallenge.enum_.PriceSegment;
 import io.kprosoftware.codechallenge.repository.VehicleBrandsRepository;
@@ -11,9 +13,18 @@ import io.kprosoftware.codechallenge.repository.VehicleBrandsRepository;
 public class InitializationTest {
   @Autowired
   private VehicleBrandsRepository vehicleBrandsRepository;
+  @Autowired
+  private ModelService modelService;
 
+  @Transactional
   public void initDB() {
-    VehicleBrand vehicleBrands = new VehicleBrand("BMW", PriceSegment.Low);
-    vehicleBrandsRepository.save(vehicleBrands);
+    VehicleBrand vehicleBrand = new VehicleBrand("BMW", PriceSegment.Low);
+    vehicleBrandsRepository.save(vehicleBrand);
+
+    Model bmwIx = new Model("BMW IX", 326, vehicleBrand);
+    Model bmwXm = new Model("BMW XM", 653, vehicleBrand);
+
+    modelService.addModel(vehicleBrand.getId(), bmwIx);
+    modelService.addModel(vehicleBrand.getId(), bmwXm);
   }
 }
