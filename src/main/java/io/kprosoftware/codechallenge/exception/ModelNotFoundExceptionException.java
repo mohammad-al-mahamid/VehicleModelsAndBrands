5778f8ad69060cbcd645583699596eaa7d0e1c75
Mohammad AL Mahamid <almahamid98@gmail.com>
2023-04-25 16:42:12 +0200

    custom Exception for Model

---
+++ src/main/java/io/kprosoftware/codechallenge/exception/ModelNotFoundExceptionException.java
@@ -0,0 +1,12 @@
+package io.kprosoftware.codechallenge.exception;
+
+public class ModelNotFoundExceptionException extends RuntimeException {
+
+  public ModelNotFoundExceptionException() {
+    super();
+  }
+
+  public ModelNotFoundExceptionException(Long id) {
+    super("Can not Found model with Id: ");
+  }
+}
