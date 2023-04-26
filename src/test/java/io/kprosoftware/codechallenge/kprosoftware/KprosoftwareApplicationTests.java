172a6a32e11e7ebc012eab96f9582add5e613e58
Mohammad AL Mahamid <almahamid98@gmail.com>
2023-04-26 17:11:40 +0200

    Test: assert controller not null

--- src/test/java/io/kprosoftware/codechallenge/kprosoftware/KprosoftwareApplicationTests.java
+++ src/test/java/io/kprosoftware/codechallenge/kprosoftware/KprosoftwareApplicationTests.java
@@ -1,12 +1,26 @@
 package io.kprosoftware.codechallenge.kprosoftware;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.kprosoftware.codechallenge.controller.ModelController;
+import io.kprosoftware.codechallenge.controller.VehicleBrandController;
 
 @SpringBootTest
 class KprosoftwareApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Autowired
+	private VehicleBrandController vehicleBrandController;
+	@Autowired
+	private ModelController modelController;
+
+	@Test
+	void contextLoads() {
+		assertThat(vehicleBrandController).isNotNull();
+		assertThat(modelController).isNotNull();
+	}
 
 }
